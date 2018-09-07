baa37922894357be7085a2b984faf7ae2a3c7b79
PingFeng233 <429125987@qq.com>
2018-09-07 16:01:30 +0800

    pingfeng
    图片转文字

---
+++ img2text/img2text.py
@@ -0,0 +1,34 @@
+from aip import AipOcr
+
+""" 你的 APPID AK SK """
+APP_ID = '11659469'
+API_KEY = 'VZaY7xlqkK8RHUiGxtm9dSLu'
+SECRET_KEY = 'HQyvmFziCxQ7TgpwvO2ZZEpGDIh8BY1H'
+
+client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+
+""" 读取图片 """
+
+
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+
+image = get_file_content('2.png')
+
+""" 调用通用文字识别, 图片参数为本地图片 """
+client.basicGeneral(image)
+
+""" 如果有可选参数 """
+options = {}
+options["language_type"] = "CHN_ENG"
+options["detect_direction"] = "true"
+options["detect_language"] = "true"
+options["probability"] = "true"
+
+""" 带参数调用通用文字识别, 图片参数为本地图片 """
+datas = client.basicGeneral(image, options)
+print(datas['words_result'])
+for data in datas['words_result']:
+    print(data['words'])
